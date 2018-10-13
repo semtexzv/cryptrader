08a388f129ede5409e9f5ea8b699808e5eb19c0c
Michal Hornický <semtexzv@gmail.com>
2018-10-13 12:59:45 +0200

    Start working on thesis text, look for literature

---
+++ thesis/watch-build.sh
@@ -0,0 +1,6 @@
+inotifywait -e close_write,moved_to,create -m . |
+while read -r directory events filename; do
+  if [[ "$filename" =~ .*\.tex ]]; then
+    make  2> >(while read line; do echo -e "\e[01;31m$line\e[0m" >&2; done)
+  fi
+done
